ed263224f7a4d371c7b184cd19f2a141f368e648
Andrew <andrew1mail@gmail.com>
2023-11-17 03:15:38 +0800

    first interval table

---
+++ app/index.tsx
@@ -0,0 +1,106 @@
+﻿import Interval from "../components/Interval";
+import React from "react";
+import uuid from "react-native-uuid";
+import { SafeAreaView, View, Text, ScrollView, FlatList } from "react-native";
+
+const timeIntervals = [1, 2, 3, 4, 5, 6, 7, 8];
+
+function randomMinutes() {
+  return Math.floor(Math.random() * 240);
+}
+
+const workers = [
+  {
+    name: "王大明",
+    start: randomMinutes(),
+    end: 240 + randomMinutes(),
+    id: uuid.v4(),
+  },
+  {
+    name: "李小美",
+    start: randomMinutes(),
+    end: 240 + randomMinutes(),
+    id: uuid.v4(),
+  },
+  {
+    name: "陳小華",
+    start: randomMinutes(),
+    end: 240 + randomMinutes(),
+    id: uuid.v4(),
+  },
+  {
+    name: "林小玉",
+    start: randomMinutes(),
+    end: 240 + randomMinutes(),
+    id: uuid.v4(),
+  },
+  {
+    name: "黃小花",
+    start: randomMinutes(),
+    end: 240 + randomMinutes(),
+    id: uuid.v4(),
+  },
+  {
+    name: "張小明",
+    start: randomMinutes(),
+    end: 240 + randomMinutes(),
+    id: uuid.v4(),
+  },
+  {
+    name: "林小明",
+    start: randomMinutes(),
+    end: 240 + randomMinutes(),
+    id: uuid.v4(),
+  },
+  {
+    name: "陳小明",
+    start: randomMinutes(),
+    end: 240 + randomMinutes(),
+    id: uuid.v4(),
+  },
+  {
+    name: "王小明",
+    start: randomMinutes(),
+    end: 240 + randomMinutes(),
+    id: uuid.v4(),
+  },
+  {
+    name: "李小明",
+    start: randomMinutes(),
+    end: 240 + randomMinutes(),
+    id: uuid.v4(),
+  },
+  {
+    name: "黃小明",
+    start: randomMinutes(),
+    end: 240 + randomMinutes(),
+    id: uuid.v4(),
+  },
+];
+
+const App = () => {
+  return (
+    <SafeAreaView className="h-screen">
+      <Text className="text-center font-bold text-4xl py-10">Scheduler</Text>
+      <View className="flex flex-row px-8 items-center">
+        <Text className="text-center font-bold text-2xl w-1/5">Name</Text>
+        <View className="flex flex-row justify-between items-center w-4/5">
+          {timeIntervals.map((time) => (
+            <Text key={time} className="text-center font-bold text-2xl">
+              {time}:00
+            </Text>
+          ))}
+        </View>
+      </View>
+      <FlatList
+        data={workers}
+        renderItem={({ item }) => (
+          <Interval name={item.name} start={item.start} end={item.end} />
+        )}
+        keyExtractor={(item) => "" + item.id}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ components/Interval.tsx
@@ -0,0 +1,64 @@
+﻿import React from "react";
+import { View, Text } from "react-native";
+
+interface IntervalProps {
+  name: string;
+  start: number;
+  end: number;
+}
+
+const timeIntervals = [1, 2, 3, 4, 5, 6, 7, 8];
+
+const Interval = ({ name, start, end }: IntervalProps) => {
+  const roundTo2DecimalPlaces = (num: number) => {
+    return Math.round(num * 100) / 100;
+  };
+
+  let coloredPortion = roundTo2DecimalPlaces(
+    ((Math.min(480, end) - Math.max(0, start)) / 480) * 100
+  );
+  let whiteLeft = roundTo2DecimalPlaces((Math.max(0, start) / 480) * 100);
+  let whiteRight = roundTo2DecimalPlaces(
+    ((480 - Math.min(480, end)) / 480) * 100
+  );
+
+  return (
+    <View className="flex flex-row px-8 items-center h-fit py-4">
+      <Text className="text-center font-bold text-2xl w-1/5">{name}</Text>
+      <View className="flex flex-[8] flex-row justify-between items-center w-4/5 h-full relative">
+        <View className="absolute flex flex-row h-full w-full bg-blue">
+          <View
+            className="h-full bg-white"
+            style={{ minWidth: `${whiteLeft}%` }}
+          ></View>
+          <View
+            className="h-full bg-red-500 border-4 border-black border-x-8 border-l-cyan-600 border-r-cyan-600"
+            style={{ minWidth: `${coloredPortion}%` }}
+          >
+            <View className=" flex flex-row justify-between items-center">
+              <Text className="mt-[-40] ml-[-10]">
+                {Math.floor(start / 60)}: {start % 60}
+              </Text>
+              <Text className="font-bold text-3xl">Job Name</Text>
+              <Text className="mt-[-40] mr-[-10]">
+                {Math.floor(end / 60)}: {end % 60} minutes
+              </Text>
+            </View>
+          </View>
+          <View
+            className="h-full bg-white"
+            style={{ minWidth: `${whiteRight}%` }}
+          ></View>
+        </View>
+        {timeIntervals.map((time) => (
+          <View
+            key={time}
+            className="border border-black flex-1 h-full flex flex-row"
+          ></View>
+        ))}
+      </View>
+    </View>
+  );
+};
+
+export default Interval;
